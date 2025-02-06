9b3bec16e35032605b0120f2c03024f762bdbacc
jaybaraiya0840 <jaybaraiya0802@gmail.com>
2025-02-06 12:17:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function stringChop(str, size) {
   // your code here
+
+	string is null
+    if (str === null) {
+        return [];
+    }
+
+    let chunks = [];
+
+    for (let i = 0; i < str.length; i += size) {
+        // Use substring to get the chunk of the specified length
+        chunks.push(str.substring(i, i + size));
+    }
+
+    return chunks;
 }
 
-// Do not change the code below
+
+
+// // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
